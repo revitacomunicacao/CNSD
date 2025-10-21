4b0e4c63de67a49778ff86c44476bd31ddf99aaf
Pedrinho Hilário <flygames1214@gmail.com>
2025-10-21 08:46:26 -0300

    alterções nas paginas de O Colégio

--- src/app/o-colegio/secretaria-academica/index.tsx
+++ src/app/o-colegio/secretaria-academica/index.tsx
@@ -35,7 +35,8 @@ export default function SecretariaDigital() {
   const [submitMessage, setSubmitMessage] = useState<{ type: 'success' | 'error', text: string } | null>(null)
   
   const formatPhone = (value: string) => {
-    const numbers = value.replace(/\D/g, '')
+    const numbers = 
+    value.replace(/\D/g, '')
     if (numbers.length <= 11) {
       return numbers.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3')
         .replace(/^(\d{2})(\d{4})(\d{0,4}).*/, '($1) $2-$3')
